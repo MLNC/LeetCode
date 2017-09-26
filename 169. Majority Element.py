79b4e003e372e209bf727f8b7767ff766977ff79
iiian <iiiantong@gmail.com>
2017-09-26 11:18:04 -0400

    33,160,169added. 9/26

---
+++ 169. Majority Element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        set = {}
+        for i in nums:
+            if i not in set:
+                set[i] = 1
+            else:
+                set[i] += 1
+            if set[i] > len(nums)/2:
+                return i
+    def majorityElementSort(self,nums):
+        return [len(nums)/2]
